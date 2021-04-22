0012da73b4be65662bb75c8e805a431a6d375a1c
Jun Jia <junjia2210@gmail.com>
2021-04-22 09:43:51 +0800

    Change 10 to n

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ from nltk.corpus import stopwords
 from collections import Counter
 
 # usage
-# python3 index.py -i 'dataset.zip' -d dictionary.txt -p postings.txt
+# python index.py -i dataset.zip -d dictionary.txt -p postings.txt
 
 
 def usage():
@@ -67,7 +67,7 @@ def build_index(in_dir, out_dict, out_postings):
 
     # For each document, we iterate through its zones and populate postings_dict and index_dict
     n = len(df.index)
-    for i in range(10):
+    for i in range(n):
         record = df.iloc[i]
         docId = record['document_id']
         print('Starting on document: {}'.format(docId))  # Log docId
